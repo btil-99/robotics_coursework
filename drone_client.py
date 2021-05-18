711dd87f32f74499163eea0ead910bcc30d1ac4c
birut <b.tilvikaite-17@student.lboro.ac.uk>
2021-05-18 15:42:11 +0000

    drone takeoff code

---
+++ drone_client.py
@@ -0,0 +1,31 @@
+import sys
+from tello_msgs.srv import TelloAction
+import rclpy
+from rclpy.node import Node
+
+class ClientAsync(Node):
+    def __init__(self):
+        super().__init__('client')
+        self.client = self.create_client(TelloAction, '/drone1/tello_action')
+        while not self.client.wait_for_service(timeout_sec=1.0):
+            print('service not available, waiting again...')
+        self.req = TelloAction.Request()
+
+    def send_request(self, command):
+        self.req.cmd = command
+        return self.client.call_async(self.req)
+
+
+rclpy.init()
+
+client = ClientAsync()
+service_return = client.send_request('takeoff')
+
+while rclpy.ok():
+    rclpy.spin_once(client)
+    if service_return.done():
+        response = service_return.result()
+        break
+
+client.destroy_node()
+rclpy.shutdown()
