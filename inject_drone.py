5d81681acf8f9492335242b11d5a1175622e8087
birut <b.tilvikaite-17@student.lboro.ac.uk>
2021-05-18 12:45:47 +0000

    injecting drone

---
+++ inject_drone.py
@@ -0,0 +1,45 @@
+import sys
+import rclpy
+import transformations
+from gazebo_msgs.srv import SpawnEntity
+from geometry_msgs.msg import Pose
+
+
+def inject(xml: str, initial_pose: Pose):
+    """Create a ROS node, and use it to call the SpawnEntity service"""
+
+    rclpy.init()
+    node = rclpy.create_node('inject_node')
+    client = node.create_client(SpawnEntity, 'spawn_entity')
+
+    if not client.service_is_ready():
+        node.get_logger().info('waiting for spawn_entity service...')
+        client.wait_for_service()
+
+    request = SpawnEntity.Request()
+    request.xml = xml
+    request.initial_pose = initial_pose
+    future = client.call_async(request)
+    rclpy.spin_until_future_complete(node, future)
+
+    if future.result() is not None:
+        node.get_logger().info('response: %r' % future.result())
+    else:
+        raise RuntimeError('exception while calling service: %r' % future.exception())
+
+    node.destroy_node()
+    rclpy.shutdown()
+
+f = open('./tello_1.urdf', 'r')
+
+p = Pose()
+p.position.x = float(1)
+p.position.y = float(0)
+p.position.z = float(0)
+q = transformations.quaternion_from_euler(0, 0, float(0))
+p.orientation.w = q[0]
+p.orientation.x = q[1]
+p.orientation.y = q[2]
+p.orientation.z = q[3]
+
+inject(f.read(), p)
