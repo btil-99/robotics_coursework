1da39fa246c5348fa895ec844209e4d340f942a3
Alex Muzzy <alexmuzzy33@gmail.com>
2021-05-20 00:27:45 +0100

    Added dataset generator script. Refactored dataset videos.

---
+++ datasets/generate_training_data.py
@@ -0,0 +1,52 @@
+import cv2
+import glob
+import argparse
+from os.path import join
+
+
+class ImageGenerator:
+
+    def __init__(self, input_directory, output_directory, number_of_frames):
+        self.video_file_paths = glob.glob(join(input_directory, '*.mp4'))
+        # Gets every video from input directory.
+
+        self.output_directory = output_directory
+        self.frame_multiplier = int(number_of_frames)
+
+    def generate_images_from_videos(self):
+        output_count = 0  # Set output file count so all images are unique.
+        for video_file_path in self.video_file_paths:
+
+            print(f"Generating dataset from {video_file_path}.")
+            cv_video_capture = cv2.VideoCapture(video_file_path)
+
+            video_frame_counter = 0
+
+            while cv_video_capture.isOpened():
+                success, image = cv_video_capture.read()
+                print(
+                    f'Frame {video_frame_counter} from {video_file_path}.')
+
+                if success:  # Check if the video has been loaded correctly.
+                    cv2.imwrite(join(
+                        self.output_directory,
+                        f"frame{output_count}.jpg"),
+                        image)
+                    # Save frame as JPEG file.
+
+                    output_count += 1
+                    video_frame_counter += 1 * self.frame_multiplier
+                    cv_video_capture.set(1, video_frame_counter)
+                else:
+                    break
+
+
+if __name__ == "__main__":
+    args = argparse.ArgumentParser()
+    args.add_argument("--input", help="Path to video directory.")
+    args.add_argument("--output", help="Path to image directory.")
+    args.add_argument("--frames", help="Frame multiplier.")
+
+    args = args.parse_args()
+    image_generator = ImageGenerator(args.input, args.output, args.frames)
+    image_generator.generate_images_from_videos()
