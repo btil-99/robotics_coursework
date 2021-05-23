6759e910b6f031531779c085e0780feb69a94b27
AlexMuzzy <alexmuzzy33@gmail.com>
2021-05-23 16:58:45 +0100

    Refactored code for further readability.

--- drone_controller.py
+++ drone_controller.py
@@ -3,14 +3,15 @@ from rclpy.node import Node
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge
 import geometry_msgs.msg
+from my_msgs.srv import Location
 from drone_object_detection import ObjectDetection
 from math import radians
 import time
 
 
-class DroneNode(Node):
+class DroneController(Node):
     def __init__(self):
-        super().__init__('drone_node')
+        super().__init__('drone_controller')
 
         self.bridge = CvBridge()
         self.object_detection = ObjectDetection(
@@ -37,11 +38,13 @@ class DroneNode(Node):
 
         self.target_time = None
         self.detection_score = None
-        self.drone_speed = 0.05
+        self.drone_speed = 0.05  # drone speed multiplier.
         self.has_initialised = False
+
+        # Set turning rate to 90 degrees a second.
         self.drone_turn = radians(90)
         self.count = 0
-        self.person_found = False
+
         # SUBSCRIBERS
         self.image_subscription = self.create_subscription(
             Image,
@@ -55,6 +58,9 @@ class DroneNode(Node):
             '/drone1/cmd_vel',
             10)
 
+        # CLIENTS
+        self.location_client = self.create_client(Location, 'person_location')
+
     def image_callback(self, image_message):
 
         cv_image = self.bridge.imgmsg_to_cv2(
@@ -63,66 +69,80 @@ class DroneNode(Node):
         self.detection_score = self.object_detection.get_detection_image(
             cv_image)
 
-        if not self.person_found:
+        if self.person_location is None:
             self.locate_person()
 
     def locate_person(self):
         twist = geometry_msgs.msg.Twist()
 
+        twist = self.rotate_and_locate_person(twist)
+
+        self.environment_rotation_timer(6)  # 6 seconds.
+
+        self.twist_publisher.publish(twist)
+
+    def rotate_and_locate_person(self, twist):
         if not self.has_initialised:
             self.logger.info(
                 '''Drone initialised. Starting via
                 moving to top left position.''')
 
+            # Rotate 45 degrees to reach first corner.
             twist.angular.z = (self.drone_turn * self.drone_speed) / 2
+
+            # Set flag to start full rotation.
             self.has_initialised = True
         else:
+
+            # Locate person based on probability threshold value.
             if self.detection_score[0] < 0.99:
+
+                # Rotate around the environment.
                 twist.angular.z = self.drone_turn * self.drone_speed
             else:
+                # Set location of person.
                 self.person_location = self.room_directions[self.count]
-                print('Person Found!')
-                self.twist_publisher.publish(geometry_msgs.msg.Twist())
-                self.person_found = True
-                print(self.person_location)
+                self.logger.info(
+                    'Person Found! Location: {}'
+                    .format(self.person_location))
+                self.location_async_request()
 
-        if self.person_location is None:
-            if self.target_time is None:
-                self.target_time = time.time() + 6
-            if time.time() >= self.target_time:
-                # count increases every time drone passes a corner
-                # and person is not found.
-                self.count += 1
-                print(self.count)
-                self.target_time = time.time() + 6
-        else:
-            print(self.person_location)
+        return twist
 
-        self.twist_publisher.publish(twist)
-
-    def rotate_drone(self):
+    def environment_rotation_timer(self, timer):
         """
-        Rotates drone within clock-wise direction along the
-        Z axis (yaw).
+        Method to implement a counter based system, where target_time given
+        is the amount of time taken to rotate to each corner.
+        The speed is calculated by using the distance (radians) and the time
+        taken to reach a certain distance.
         """
 
-        twist = geometry_msgs.msg.Twist()
-        twist.angular.z = self.drone_speed   # Radians per second.
+        # If timer has not been initialised yet.
         if self.target_time is None:
-            self.logger.info('Timer initialising.')
-            self.target_time = time.time() + 21
-            self.twist_publisher.publish(twist)
-
+            self.target_time = time.time() + timer
         if time.time() >= self.target_time:
-            self.logger.info('Drone is facing top of the room.')
-            self.twist_publisher.publish(geometry_msgs.msg.Twist())
-            time.sleep(5)
-            self.twist_publisher.publish(twist)
-            self.target_time = time.time() + 20
+
+            # Count increases every time drone passes
+            # a corner and person is not found.
+            self.count += 1
+            self.target_time = time.time() + timer
+
+    def location_async_request(self):
+        while not self.location_client.wait_for_service(timeout_sec=1.0):
+            self.logger.info(
+                '''Location service not
+                 available, waiting again...''')
+
+        self.logger.info('Submitting location of person to turtlebot.')
+        request = Location().Request()
+        request.location_x, request.location_y = self.person_location
+        # Submit an Asynchronous request to the location service to invoke
+        # the DQN Gazebo node.
+        self.location_client.call_async(request)
 
 
 rclpy.init()
-drone_controller = DroneNode()
+drone_controller = DroneController()
 
 rclpy.spin(drone_controller)
 
